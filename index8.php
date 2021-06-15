f29b7000c448d60b398c1bf54dfd12a19bd20cd7
Juanda1992 <61318380+Juanda1992@users.noreply.github.com>
2021-06-14 21:28:08 -0500

    Uptade

--- index8.php
+++ index8.php
@@ -1,5 +1,35 @@
 <?php
+    // Las clases abstractas obligan a la clase que los extiende usar obligatoriamente
+    // los metodos asignados en ella
+    
+    abstract class Molde{
 
+        abstract public function ingresarNombre($nombre);
+        abstract public function obtenerNombre();
+
+    }
+
+    class Persona extends Molde{
+
+        private $mensaje ="Hello ma people";
+        private $nombre;
+
+        public function mostrar(){
+            print $this->mensaje;
+        }
+
+        public function ingresarNombre($nombre){
+            $this->nombre= $nombre;
+
+        }
+
+        public function obtenerNombre(){
+            print $this->nombre;
+        }
+    }
+
+    $obj = new Persona();
+    $obj->mostrar();
 
 
 ?>
